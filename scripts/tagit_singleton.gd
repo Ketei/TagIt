f2c99b1245ba8fb068042ca97dbadf20e9c082c3
Ketei <ketei.ilust@gmail.com>
2025-03-23 02:48:25 -0600

    bumped version

--- scripts/tagit_singleton.gd
+++ scripts/tagit_singleton.gd
@@ -20,7 +20,7 @@ signal website_deleted(site_id: int)
 const DATABASE_PATH: String = "user://tag_database.db"
 const SEARCH_WILDCARD: String = "*"
 const DB_VERSION: int = 3
-const TAGIT_VERSION_ARRAY: Array[int] = [3,3,5]
+const TAGIT_VERSION_ARRAY: Array[int] = [3, 3, 6]
 const MAX_PARENT_RECURSION: int = 100
 const IMAGE_LIMITS: Vector2i = Vector2i(1000, 1000)
 const LEV_DISTANCE: float = 0.75
