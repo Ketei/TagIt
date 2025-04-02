e78531d0210bc126324198e80ec7785f3f71e669
Ketei <ketei.ilust@gmail.com>
2025-04-02 14:41:01 -0600

    Esc now closes the update window

--- scenes/help_window.gd
+++ scenes/help_window.gd
@@ -6,6 +6,8 @@ signal close_pressed
 const konami_code = [KEY_UP, KEY_UP, KEY_DOWN, KEY_DOWN, KEY_LEFT, KEY_RIGHT, KEY_LEFT, KEY_RIGHT, KEY_B, KEY_A]
 var input_index = 0
 
+var konami: bool = false
+
 @onready var version_label: Label = $MainPanel/DataContainer/LabelsContainer/VersionLabel
 @onready var close_button: Button = $MainPanel/DataContainer/LabelsContainer/TitlePanel/CloseButton
 @onready var portrait_rect: TextureRect = $MainPanel/DataContainer/PortraitRect
@@ -22,17 +24,18 @@ func _ready() -> void:
 	get_viewport().gui_release_focus()
 	version_label.text = ".".join(DataManager.TAGIT_VERSION_ARRAY)
 	close_button.pressed.connect(close_pressed.emit)
-	
-
 
 
 func _input(event):
-	if event is InputEventKey and event.pressed:
-		if event.keycode == konami_code[input_index]:
-			input_index += 1
-			if input_index == konami_code.size():
-				portrait_rect.texture = preload("res://textures/ready.png")
-				set_process_input(false)
+	if event is InputEventKey and event.is_pressed() and not event.is_echo():
+		if not konami:
+			if event.keycode == konami_code[input_index]:
+				input_index += 1
+				if input_index == konami_code.size():
+					portrait_rect.texture = preload("res://textures/ready.png")
+					konami = true
+			else:
 				input_index = 0
-		else:
-			input_index = 0 
+		if event.is_action(&"ui_cancel"):
+			close_pressed.emit()
+			get_viewport().set_input_as_handled()
