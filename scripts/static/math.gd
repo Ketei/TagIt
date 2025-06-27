63cc8ac2e81991b9ae3d1f80a40bb6f8b47bc739
Ketei <ketei.ilust@gmail.com>
2025-06-26 19:41:50 -0600

    misc

--- scripts/static/math.gd
+++ scripts/static/math.gd
@@ -7,3 +7,10 @@ static func is_between(what: float, between_a: float, between_b: float) -> bool:
 	var max_value = maxf(between_b, between_b)
 	
 	return min_value <= what and what <= max_value
+
+
+static func is_betweeni(what: int, between_a: int, between_b: int) -> bool:
+	var min_value = mini(between_a, between_b)
+	var max_value = maxi(between_b, between_b)
+	
+	return min_value <= what and what <= max_value
